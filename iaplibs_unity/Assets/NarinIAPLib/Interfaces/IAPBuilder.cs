e9ecc8a8c9fe8bae4f9e13b1ee8a770b7f64167e
e.foadian <e.foadian@narin.co>
2020-07-19 10:15:12 +0430

    - Documentation IAPBuilder and IIAPManager classes

--- iaplibs_unity/Assets/NarinIAPLib/Interfaces/IAPBuilder.cs
+++ iaplibs_unity/Assets/NarinIAPLib/Interfaces/IAPBuilder.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 namespace Narin.Unity.IAP {
 
     public partial class IAPBuilder {
+        /// <summary>
+        /// The reference of last builded IIAPManager object
+        /// </summary>
         public static IIAPManager CurrentIapManager { get; private set; } = null;
 
         private Dictionary<Store, string> _publicKeys = new Dictionary<Store, string>();
         private Dictionary<string, ProductBase> _products = new Dictionary<string, ProductBase>();
 
+        /// <summary>
+        /// Set public key for each store (needed only for Bazaar and Myket)
+        /// </summary>
+        /// <param name="store"></param>
+        /// <param name="publicKey"></param>
+        /// <returns></returns>
         public IAPBuilder SetPublicKey(Store store, string publicKey) { 
             _publicKeys.Add(store, publicKey);
             return this;
         }
-
+        /// <summary>
+        /// Set new product for all store (you must set all products before init IAP system)
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public IAPBuilder AddProduct(string productId, ProductType type) {
             _products.Add(productId, new ProductBase(productId, type));
             return this;
         }
 
+        /// <summary>
+        /// Set new product with specific productId for each store
+        /// </summary>
+        /// <param name="aliasProductId"></param>
+        /// <param name="productIds"></param>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public IAPBuilder AddProduct(string aliasProductId, Dictionary<Store, string> productIds, ProductType type) {
             string pid = null;
 
@@ -38,7 +59,11 @@ namespace Narin.Unity.IAP {
             _products.Add(aliasProductId, new ProductBase(pid, type));
             return this;
         }
-
+        /// <summary>
+        /// Build IIAPManager object and attach it to GameObject (After calling this method the IAPBuilder class is reset)
+        /// </summary>
+        /// <param name="mono">Mono reference from GameObject to which you want to attach IIAPManager</param>
+        /// <returns></returns>
         public IIAPManager BuildAndAttach(MonoBehaviour mono) {
             IIAPManager ret = null;
 
--- iaplibs_unity/Assets/NarinIAPLib/Interfaces/IIAPManager.cs
+++ iaplibs_unity/Assets/NarinIAPLib/Interfaces/IIAPManager.cs
@@ -10,21 +10,69 @@ namespace Narin.Unity.IAP {
     }
 
     public interface IIAPManager {
+        /// <summary>
+        /// Fired after Init is called when IAP is supported on the devive
+        /// </summary>
         event EventHandler<EventArgs>               OnPurchaseSupported;
+        /// <summary>
+        /// Fired after Init is called when IAP is not supported on the device
+        /// </summary>
         event EventHandler<ErrorEventArgs>          OnPurchaseNotSupported;
+        /// <summary>
+        /// Fired after PurchaseProduct is called when a purchase succeeds
+        /// </summary>
         event EventHandler<PurchaseEventArgs>       OnPurchaseSucceeded;
+        /// <summary>
+        /// Fired after PurcahseProduct is called when a purchase fails
+        /// </summary>
         event EventHandler<ErrorEventArgs>          OnPurchaseFailed;
+        /// <summary>
+        /// Fired after QuerySkuInfo is called when query has returned
+        /// </summary>
         event EventHandler<QuerySkuInfoEventArgs>   OnQuerySkuInfoSucceeded;
+        /// <summary>
+        /// Fired after QuerySkuInfo is called when query fails
+        /// </summary>
         event EventHandler<ErrorEventArgs>          OnQuerySkuInfoFailed;
+        /// <summary>
+        /// Fired after ConsumeProduct is called when a consume succeeds
+        /// </summary>
         event EventHandler<PurchaseEventArgs>       OnConsumeSucceeded;
+        /// <summary>
+        /// Fired after ConsumeProduct is called when a consume fails
+        /// </summary>
         event EventHandler<ErrorEventArgs>          OnConsumeFailed;
+        /// <summary>
+        /// !!! This event not implemented !!!
+        /// </summary>
         event EventHandler<EventArgs>               OnRetriveFailedPurchasesSucceeded;
+        /// <summary>
+        /// !!! This event not implemented !!!
+        /// </summary>
         event EventHandler<ErrorEventArgs>          OnRetriveFailedPurchasesFailed;
 
+        /// <summary>
+        /// Initializes IAP system
+        /// </summary>
         void Init();
+        /// <summary>
+        /// Sends a request to purchase the product
+        /// </summary>
+        /// <param name="productId"></param>
         void PurchaseProduct(string productId);
+        /// <summary>
+        /// Sends a request to consume the product
+        /// </summary>
+        /// <param name="productId"></param>
         void ConsumeProduct(string productId);
+        /// <summary>
+        /// Sends a request to get all product information as setup in the store panel
+        /// </summary>
+        /// <param name="productIds"></param>
         void QuerySkuInfo(string[] productIds);
+        /// <summary>
+        /// !!! This method not implemented !!!
+        /// </summary>
         void RetrieveFailedPurchases();
     }
 }
